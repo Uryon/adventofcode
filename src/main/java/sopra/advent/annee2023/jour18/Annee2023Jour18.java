b26bfd59c24d5a4b2d9e8c3b9246e63a74ceb6b6
vryckewaert <valentin.ryckewaert@soprasteria.com>
2025-02-25 10:51:56 +0100

    [FEAT] Jour 18 fonctionnel

--- src/main/java/sopra/advent/annee2023/jour18/Annee2023Jour18.java
+++ src/main/java/sopra/advent/annee2023/jour18/Annee2023Jour18.java
@@ -22,7 +22,7 @@ public class Annee2023Jour18 extends Jour {
                 case "D" -> Direction.SUD;
                 default -> throw new AdventException("Unexpected value: " + m.group(1));
             };
-            int distance = Integer.parseInt(m.group(2));
+            long distance = Long.parseLong(m.group(2));
             digs.add(new Dig(direction, distance));
         }
         return execute(digs);
@@ -31,26 +31,26 @@ public class Annee2023Jour18 extends Jour {
     public Object executePartie2(List<String> in) {
         List<Dig> digs = new ArrayList<>();
         for (String ligne : in) {
-            Matcher m = Utils.genererMatcherAndFind("(.) (\\d+) \\(#(.*)(.)\\)", ligne);
-            Direction direction = switch (m.group(4)) {
+            Matcher m = Utils.genererMatcherAndFind("\\(#([0-9a-fA-F]{5})([0-3])\\)", ligne);
+            Direction direction = switch (m.group(2)) {
                 case "0" -> Direction.EST;
                 case "2" -> Direction.OUEST;
                 case "3" -> Direction.NORD;
                 case "1" -> Direction.SUD;
-                default -> throw new AdventException("Unexpected value: " + m.group(1));
+                default -> throw new AdventException("Unexpected value: " + m.group(2));
             };
-            int distance = Integer.parseInt(m.group(3), 16);
+            long distance = Long.parseLong(m.group(1), 16);
             digs.add(new Dig(direction, distance));
         }
         return execute(digs);
     }
 
-    private int execute(List<Dig> digs) {
-        int x = 0;
-        int y = 0;
-        List<int[]> trous = new ArrayList<>();
-        trous.add(new int[]{x, y});
-        int perimetre = 0;
+    private long execute(List<Dig> digs) {
+        long x = 0;
+        long y = 0;
+        List<long[]> trous = new ArrayList<>();
+        trous.add(new long[]{x, y});
+        long perimetre = 0;
 
         for (Dig dig : digs) {
             switch (dig.direction) {
@@ -68,29 +68,29 @@ public class Annee2023Jour18 extends Jour {
                     break;
             }
 
-            trous.add(new int[]{x, y});
+            trous.add(new long[]{x, y});
             perimetre += dig.distance;
 
         }
         // Aire calculée avec la formule du Shoelace car plus propre que faire à la main
-        int aire = 0;
+        long aire = 0;
         for (int i = 0; i < trous.size() - 1; i++) {
-            int[] a = trous.get(i);
-            int[] b = trous.get(i + 1);
+            long[] a = trous.get(i);
+            long[] b = trous.get(i + 1);
             aire += (a[0] * b[1]) - (b[0] * a[1]);
         }
         aire = Math.abs(aire) / 2;
 
         // Utilisation de la formule de Pick
-        int nbPointsInterieurs = aire - (perimetre / 2) + 1;
+        long nbPointsInterieurs = aire - (perimetre / 2) + 1;
         return nbPointsInterieurs + perimetre;
     }
 
     public class Dig {
         private final Direction direction;
-        private final int distance;
+        private final long distance;
 
-        public Dig(Direction direction, int distance) {
+        public Dig(Direction direction, long distance) {
             this.direction = direction;
             this.distance = distance;
         }
@@ -99,8 +99,16 @@ public class Annee2023Jour18 extends Jour {
             return direction;
         }
 
-        public int getDistance() {
+        public long getDistance() {
             return distance;
+        }
+
+        @Override
+        public String toString() {
+            return "Dig{" +
+                    "direction=" + direction +
+                    ", distance=" + distance +
+                    '}';
         }
     }
 
